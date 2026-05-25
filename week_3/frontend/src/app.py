c382740d38137f2eda56b34d89462bf1732d8f06
Wesley Laptop <westae0309@gmail.com>
2026-05-25 16:07:38 +0800

    feat: added chat frontend w bootstrap

--- week_3/frontend/src/app.py
+++ week_3/frontend/src/app.py
@@ -1,14 +1,17 @@
 from pathlib import Path
 
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import os
 
 
 app = FastAPI()
 
 BASE_DIR = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+load_dotenv()
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -16,4 +19,7 @@ async def read_root(request: Request) -> HTMLResponse:
     return templates.TemplateResponse(
         request=request,
         name="chat_page.html",
+        context={
+            "backend_url": os.getenv("BACKEND_URL", ""),
+        },
     )
